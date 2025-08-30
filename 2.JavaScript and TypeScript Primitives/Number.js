f5a3a12f56dd29921d0d63d7d73c8b042c4bdcc9
itorly <itorly@163.com>
2025-08-30 18:02:00 +0800

    Number

---
+++ 2.JavaScript and TypeScript Primitives/Number.js
@@ -0,0 +1,10 @@
+var decimal = 6;
+var hex = 0xf00d; // Hexadecimal
+var binary = 10; // Binary
+var octal = 484; // Octal
+var float = 3.14; // Floating point
+console.log("decimal: " + decimal);
+console.log("hex: " + hex);
+console.log("binary: " + binary);
+console.log("octal: " + octal);
+console.log("float: " + float);
---
+++ 2.JavaScript and TypeScript Primitives/Number.ts
@@ -0,0 +1,11 @@
+let decimal: number = 6;
+let hex: number = 0xf00d;       // Hexadecimal
+let binary: number = 0b1010;     // Binary
+let octal: number = 0o744;      // Octal
+let float: number = 3.14;      // Floating point
+
+console.log("decimal: " + decimal);
+console.log("hex: " + hex);
+console.log("binary: " + binary);
+console.log("octal: " + octal);
+console.log("float: " + float);

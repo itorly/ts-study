2205e39c6994338c80d0345e6f3896689af26202
itorly <itorly@163.com>
2025-08-30 18:01:48 +0800

    2.JavaScript and TypeScript Primitives: Boolean

---
+++ 2.JavaScript and TypeScript Primitives/Boolean.js
@@ -0,0 +1,4 @@
+var isActive = true;
+var hasPermission = false; // TypeScript infers 'boolean' type
+console.log(isActive); // true
+console.log(hasPermission); // false
---
+++ 2.JavaScript and TypeScript Primitives/Boolean.ts
@@ -0,0 +1,5 @@
+let isActive: boolean = true;
+let hasPermission = false; // TypeScript infers 'boolean' type
+
+console.log(isActive); // true
+console.log(hasPermission); // false

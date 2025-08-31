eb122839f40fb1c447d7607a9926d0cd7331dfe6
itorly <itorly@163.com>
2025-08-31 23:37:13 +0800

    Type Alias

---
+++ src/11.Functions/Type Alias.ts
@@ -0,0 +1,6 @@
+type Negate = (value: number) => number;
+
+// in this function, the parameter `value` automatically gets assigned the type `number` from the type `Negate`
+const negateFunction: Negate = (value) => value * -1;
+
+console.log(negateFunction(10)); // -10

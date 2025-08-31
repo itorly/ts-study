6e97c2d789272566ed0bc553013467dc2f738887
itorly <itorly@163.com>
2025-08-31 23:21:41 +0800

    Example: Union and Intersection Types

---
+++ src/9.Type Aliases and Interfaces/Example- Union and Intersection Types.ts
@@ -0,0 +1,6 @@
+type Animal = { name: string };
+type Bear = Animal & { honey: boolean };
+const bear: Bear = { name: "Winnie", honey: true };
+
+type Status = "success" | "error";
+let response: Status = "success"; // response can only be "success" or "error"

4f7a21205ed48b557abab591ce52d4a167db44a7
itorly <itorly@163.com>
2025-08-31 21:15:20 +0800

    4.Special Types: Example without any

---
+++ src/4.Special Types/Example without any.ts
@@ -0,0 +1,6 @@
+let u = true;
+// u = "string"; // Error: Type 'string' is not assignable to type 'boolean'.
+
+// u.runANonExistentMethod(); // Error: Property 'runANonExistentMethod' does not exist on type 'boolean'.
+
+// console.log(Math.round(u)); // Error: Argument of type 'boolean' is not assignable to parameter of type 'number'.

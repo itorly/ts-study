8048bf20f51c25f65715be82701ab399ab18032e
itorly <itorly@163.com>
2025-08-31 22:46:28 +0800

    trying to set them in the wrong order will throw an error

---
+++ src/6.Tuples/trying to set them in the wrong order will throw an error.ts
@@ -0,0 +1,7 @@
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize incorrectly throws an error
+// ourTuple = [false, 'Coding God was mistaken', 5];
+
+// console.log(ourTuple);

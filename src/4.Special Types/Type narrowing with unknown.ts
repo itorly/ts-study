c46cacd4d7753abf4f4627927b300460552c56bf
itorly <itorly@163.com>
2025-08-31 21:41:44 +0800

    Type narrowing with unknown

---
+++ src/4.Special Types/Type narrowing with unknown.ts
@@ -0,0 +1,12 @@
+function processValue(value: unknown) {
+  if (typeof value === 'string') {
+    // value is now treated as string
+    console.log(value.toUpperCase());
+  } else if (Array.isArray(value)) {
+    // value is now treated as any[]
+    console.log(value.length);
+  }
+}
+
+processValue('hello'); // Output: HELLO
+processValue([1, 2, 6]); // Output: 3

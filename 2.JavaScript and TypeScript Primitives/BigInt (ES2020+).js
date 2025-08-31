ccc4d4e1629c27a28c26341411b0defd27e4a0a4
itorly <itorly@163.com>
2025-08-31 16:08:17 +0800

    BigInt (ES2020+)

---
+++ 2.JavaScript and TypeScript Primitives/BigInt (ES2020+).js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const bigNumber = 9007199254740991n;
+const hugeNumber = BigInt(9007199254740991); // Alternative syntax
+console.log("bigNumber: " + bigNumber); // bigNumber: 9007199254740991n
+console.log("hugeNumber: " + hugeNumber); // hugeNumber: 9007199254740991
+//# sourceMappingURL=BigInt%20(ES2020+).js.map
---
+++ 2.JavaScript and TypeScript Primitives/BigInt (ES2020+).ts
@@ -0,0 +1,5 @@
+const bigNumber: bigint = 9007199254740991n;
+const hugeNumber = BigInt(9007199254740991); // Alternative syntax
+
+console.log("bigNumber: " + bigNumber); // bigNumber: 9007199254740991n
+console.log("hugeNumber: " + hugeNumber); // hugeNumber: 9007199254740991

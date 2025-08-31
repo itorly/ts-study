5ee3706a0f1c1278e65be775d9abc8ec9d5e825e
itorly <itorly@163.com>
2025-08-31 13:51:48 +0800

    String

---
+++ 2.JavaScript and TypeScript Primitives/String.js
@@ -0,0 +1,8 @@
+var color = "blue";
+var fullName = 'John Doe';
+var age = 30;
+var sentence = "Hello, my name is ".concat(fullName, " and I'll be ").concat(age + 1, " next year.");
+console.log("color: " + color);
+console.log("fullName: " + fullName);
+console.log("age: " + age);
+console.log("sentence: " + sentence);
---
+++ 2.JavaScript and TypeScript Primitives/String.ts
@@ -0,0 +1,9 @@
+let color: string = "blue";
+let fullName: string = 'John Doe';
+let age: number = 30;
+let sentence: string = `Hello, my name is ${fullName} and I'll be ${age + 1} next year.`;
+
+console.log("color: " + color);
+console.log("fullName: " + fullName);
+console.log("age: " + age);
+console.log("sentence: " + sentence);

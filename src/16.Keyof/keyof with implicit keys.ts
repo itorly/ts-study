739951869fb713cad5f515373758514554d57a88
itorly <itorly@163.com>
2025-09-01 23:59:57 +0800

    keyof with implicit keys

---
+++ src/16.Keyof/keyof with implicit keys.ts
@@ -0,0 +1,8 @@
+type StringMap = { [key: string]: unknown };
+
+// `keyof StringMap` resolves to `string` here
+function createStringPair(property: keyof StringMap, value: string): StringMap {
+  return { [property]: value };
+}
+
+console.log(JSON.stringify(createStringPair('greeting', 'hello')));

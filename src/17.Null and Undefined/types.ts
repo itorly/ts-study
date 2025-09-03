5c21d7111c099cd18e4651e6618101bca71ddac5
itorly <itorly@163.com>
2025-09-03 23:04:46 +0800

    17.Null and Undefined\types.ts

---
+++ src/17.Null and Undefined/types.ts
@@ -0,0 +1,8 @@
+let value: string | undefined | null = null;
+console.log(typeof value);    // output: object
+
+value = 'hello';
+console.log(typeof value);    // output: string
+
+value = undefined;
+console.log(typeof value);  // output: undefined

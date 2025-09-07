ff1d380789128d039159fbb0fd3c8089865da37f
itorly <itorly@163.com>
2025-09-07 23:30:02 +0800

    Remove 'readonly' and '?' modifiers

--- src/24.Advanced Types/24.1.Mapped Types/Mapped Type Modifiers.ts
+++ src/24.Advanced Types/24.1.Mapped Types/Mapped Type Modifiers.ts
@@ -9,7 +9,22 @@ type OptionalTodo = {
   [K in keyof Todo]?: Todo[K];
 };
 
+// Remove 'readonly' and '?' modifiers
+// -readonly (prefix) — remove the readonly modifier from each property.
+// -? (suffix after the index) — remove the ? (optional) modifier, i.e., make the property required.
+type Concrete<T> = {
+  -readonly [K in keyof T]-?: T[K];
+};
+
+
 const t1: OptionalTodo = { title: "Learn TS" };
 console.log(t1.title);
 console.log(String(t1.description));
 console.log(String(t1.completed));
+
+const t2: Concrete<OptionalTodo> = { title: "Learn TS", description: "Learn TS destription", completed: false };
+console.log(t2.title);
+console.log(String(t2.description));
+console.log(String(t2.completed));
+t2.title = "Modify Learn TS";
+console.log(t2.title);
